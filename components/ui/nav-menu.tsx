0b21e8917f0685ca3cb481159c06186bd4390c0f
AlexeyYakovec <yakovec8952@gmail.com>
2024-09-02 18:28:16 +0500

    fixed mobile layout

--- components/ui/nav-menu.tsx
+++ components/ui/nav-menu.tsx
@@ -37,7 +37,7 @@ export function MainNav() {
                <NavigationMenuItem>
                   <NavigationMenuTrigger>Posts</NavigationMenuTrigger>
                   <NavigationMenuContent>
-                     <ul className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px]">
+                     <ul className="grid w-[350px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px]">
                         {POSTS.map((component) => (
                            <ListItem
                               key={component.title}
