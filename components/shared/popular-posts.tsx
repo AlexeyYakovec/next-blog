b1557f886137bae776fdfa50c09111969e0d7173
unknown <yakovec8952@gmail.com>
2024-09-08 23:39:54 +0500

    newsletter with actions

--- components/shared/popular-posts.tsx
+++ components/shared/popular-posts.tsx
@@ -1,44 +1,41 @@
-"use client";
+'use client';
 
 // import { popularPosts } from "../../lib/placeholder-data";
-import { cn, fetchUrl, fetcher } from "@/lib/utils";
-import React from "react";
-import { Icons } from "../ui";
-import useSWR from "swr";
-import Link from "next/link";
-import { PopularPostsSkeleton } from "./popular-posts-skeleton";
+import { cn, fetchUrl, fetcher } from '@/lib/utils';
+import React from 'react';
+import { Icons } from '../ui';
+import useSWR from 'swr';
+import Link from 'next/link';
+import { PopularPostsSkeleton } from './popular-posts-skeleton';
 
 interface Props {
-   className?: string;
+    className?: string;
 }
 
 export const PopularPosts: React.FC<Props> = ({ className }) => {
-   const { data, error, isLoading } = useSWR(fetchUrl, fetcher);
-   console.log(data);
+    const { data, error, isLoading } = useSWR(fetchUrl, fetcher);
 
-   if (error) return <div>Failed to to load</div>;
-   if (isLoading) return <PopularPostsSkeleton />;
+    if (error) return <div>Failed to to load</div>;
+    if (isLoading) return <PopularPostsSkeleton />;
 
-   return (
-      <ul className={cn("overflow-auto ", className)}>
-         {data?.map(
-            (post: { category: string; slug: string; title: string }) => {
-               return (
-                  <Link
-                     href={`/blog/${post.category}/${post.slug}`}
-                     key={post.slug}
-                  >
-                     <li
-                        key={post.title}
-                        className="flex items-center gap-2 cursor-pointer py-2 group hover:opacity-60 transition-all"
-                     >
-                        <Icons.arrowRigth className="h-6 w-6 group-hover:translate-x-1 transition-all" />
-                        <p>{post.title}</p>
-                     </li>
-                  </Link>
-               );
-            }
-         )}
-      </ul>
-   );
+    return (
+        <ul className={cn('overflow-auto ', className)}>
+            {data?.map(
+                (post: { category: string; slug: string; title: string }) => {
+                    return (
+                        <Link
+                            href={`/blog/${post.category}/${post.slug}`}
+                            key={post.slug}>
+                            <li
+                                key={post.title}
+                                className="flex items-center gap-2 cursor-pointer py-2 group hover:opacity-60 transition-all">
+                                <Icons.arrowRigth className="h-6 w-6 group-hover:translate-x-1 transition-all" />
+                                <p>{post.title}</p>
+                            </li>
+                        </Link>
+                    );
+                },
+            )}
+        </ul>
+    );
 };
--- components/ui/footer.tsx
+++ components/ui/footer.tsx
@@ -1,121 +1,119 @@
-import { cn } from "@/lib/utils";
-import React from "react";
-import { Button, Icons, Input } from ".";
-import Link from "next/link";
-import { POSTS } from "@/lib/constants";
+import { cn } from '@/lib/utils';
+import React from 'react';
+import { Button, Icons, Input } from '.';
+import Link from 'next/link';
+import { POSTS } from '@/lib/constants';
+import { createSubscribe } from '@/lib/actions';
 
 interface Props {
-   className?: string;
+    className?: string;
 }
 
 export const Footer: React.FC<Props> = ({ className }) => {
-   return (
-      <footer
-         className={cn("bg-gray-100 py-8 dark:bg-gray-800", className)}
-         style={{ marginTop: 100 }}
-      >
-         <div className="container mx-auto px-4 md:px-6">
-            <div className="grid grid-cols-1 gap-8 md:grid-cols-4">
-               <div className="space-y-4">
-                  <div className="flex items-center space-x-2">
-                     <Icons.logo className="h-6 w-6" />
-                     <span className="text-md font-semibold">Coding como</span>
-                  </div>
-                  <p className="text-gray-500 dark:text-gray-400 text-sm">
-                     Stay Up to Date with the latest news and insights from out
-                     blog.
-                  </p>
-                  <div className="flex space-x-4 ">
-                     <a
-                        href="https://github.com/AlexeyYakovec"
-                        target="_blank"
-                        aria-label="Github"
-                        className="flex items-center gap-2 text-gray-500 hover:text-gray-700 dark:text-gray-400 dark:hover:text-gray-300 transition-all"
-                     >
-                        <Icons.github className="h-6 w-6" />
-                     </a>
-                     <a
-                        href="https://vk.com/id615720527"
-                        target="_blank"
-                        aria-label="Github"
-                        className="flex items-center gap-2 text-gray-500 hover:text-gray-700 dark:text-gray-400 dark:hover:text-gray-300 transition-all"
-                     >
-                        <Icons.vkontakte className="h-6 w-6" />
-                     </a>
-                  </div>
-               </div>
-               <div className="space-y-4">
-                  <h3 className="text-md font-semibold">Blog</h3>
-                  <ul className="space-y-2 text-sm">
-                     {POSTS.map((post) => {
-                        return (
-                           <li key={post.title}>
-                              <Link
-                                 href={post.href}
-                                 className="text-gray-500 hover:text-gray-700 dark:text-gray-400 dark:hover:text-gray-300 transition-all"
-                              >
-                                 {post.title}
-                              </Link>
-                           </li>
-                        );
-                     })}
-                  </ul>
-               </div>
+    return (
+        <footer
+            className={cn('bg-gray-100 py-8 dark:bg-gray-800', className)}
+            style={{ marginTop: 100 }}>
+            <div className="container mx-auto px-4 md:px-6">
+                <div className="grid grid-cols-1 gap-8 md:grid-cols-4">
+                    <div className="space-y-4">
+                        <div className="flex items-center space-x-2">
+                            <Icons.logo className="h-6 w-6" />
+                            <span className="text-md font-semibold">
+                                Coding como
+                            </span>
+                        </div>
+                        <p className="text-gray-500 dark:text-gray-400 text-sm">
+                            Stay Up to Date with the latest news and insights
+                            from out blog.
+                        </p>
+                        <div className="flex space-x-4 ">
+                            <a
+                                href="https://github.com/AlexeyYakovec"
+                                target="_blank"
+                                aria-label="Github"
+                                className="flex items-center gap-2 text-gray-500 hover:text-gray-700 dark:text-gray-400 dark:hover:text-gray-300 transition-all">
+                                <Icons.github className="h-6 w-6" />
+                            </a>
+                            <a
+                                href="https://vk.com/id615720527"
+                                target="_blank"
+                                aria-label="Github"
+                                className="flex items-center gap-2 text-gray-500 hover:text-gray-700 dark:text-gray-400 dark:hover:text-gray-300 transition-all">
+                                <Icons.vkontakte className="h-6 w-6" />
+                            </a>
+                        </div>
+                    </div>
+                    <div className="space-y-4">
+                        <h3 className="text-md font-semibold">Blog</h3>
+                        <ul className="space-y-2 text-sm">
+                            {POSTS.map((post) => {
+                                return (
+                                    <li key={post.title}>
+                                        <Link
+                                            href={post.href}
+                                            className="text-gray-500 hover:text-gray-700 dark:text-gray-400 dark:hover:text-gray-300 transition-all">
+                                            {post.title}
+                                        </Link>
+                                    </li>
+                                );
+                            })}
+                        </ul>
+                    </div>
 
-               <div className="space-y-4">
-                  <h3>Links</h3>
-                  <ul className="space-y-2 text-sm">
-                     <li>
-                        <a
-                           href="mailto:alekseyyakovets@gmail.com"
-                           className="text-gray-500 hover:text-gray-700 dark:text-gray-400 dark:hover:text-gray-300 transition-all"
-                        >
-                           Contact
-                        </a>
-                     </li>
-                     <li>
-                        <Link
-                           href={"/terms-of-services"}
-                           className="text-gray-500 hover:text-gray-700 dark:text-gray-400 dark:hover:text-gray-300 transition-all"
-                        >
-                           Terms of Services
-                        </Link>
-                     </li>
-                     <li>
-                        <Link
-                           href={"/privacy-policy"}
-                           className="text-gray-500 hover:text-gray-700 dark:text-gray-400 dark:hover:text-gray-300 transition-all"
-                        >
-                           Privacy Policy
-                        </Link>
-                     </li>
-                  </ul>
-               </div>
+                    <div className="space-y-4">
+                        <h3>Links</h3>
+                        <ul className="space-y-2 text-sm">
+                            <li>
+                                <a
+                                    href="mailto:alekseyyakovets@gmail.com"
+                                    className="text-gray-500 hover:text-gray-700 dark:text-gray-400 dark:hover:text-gray-300 transition-all">
+                                    Contact
+                                </a>
+                            </li>
+                            <li>
+                                <Link
+                                    href={'/terms-of-services'}
+                                    className="text-gray-500 hover:text-gray-700 dark:text-gray-400 dark:hover:text-gray-300 transition-all">
+                                    Terms of Services
+                                </Link>
+                            </li>
+                            <li>
+                                <Link
+                                    href={'/privacy-policy'}
+                                    className="text-gray-500 hover:text-gray-700 dark:text-gray-400 dark:hover:text-gray-300 transition-all">
+                                    Privacy Policy
+                                </Link>
+                            </li>
+                        </ul>
+                    </div>
 
-               <div className="space-y-4">
-                  <h3 className="text-md font-semibold">Newsletter</h3>
-                  <p className="text-gray-500 dark:text-gray-400 text-sm">
-                     Subscribe to our newsletter to stay up-to-date with the
-                     latest news and updates.
-                  </p>
+                    <div className="space-y-4">
+                        <h3 className="text-md font-semibold">Newsletter</h3>
+                        <p className="text-gray-500 dark:text-gray-400 text-sm">
+                            Subscribe to our newsletter to stay up-to-date with
+                            the latest news and updates.
+                        </p>
 
-                  <form>
-                     <div className="flex space-x-2">
-                        <Input
-                           type="email"
-                           placeholder="Enter your email"
-                           className="flex-1"
-                        />
-                        <Button>Subscribe</Button>
-                     </div>
-                  </form>
-               </div>
+                        <form action={createSubscribe}>
+                            <div className="flex space-x-2">
+                                <Input
+                                    type="email"
+                                    name="email"
+                                    id="email"
+                                    placeholder="Enter your email"
+                                    className="flex-1"
+                                />
+                                <Button>Subscribe</Button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+
+                <div className="mt-8 border-gray-200 pt-4 text-center text-xs text-gray-500 dark:border-gray-700 dark:text-gray-400">
+                    &copy; 2024 Coding como. All rights reserved.
+                </div>
             </div>
-
-            <div className="mt-8 border-gray-200 pt-4 text-center text-xs text-gray-500 dark:border-gray-700 dark:text-gray-400">
-               &copy; 2024 Coding como. All rights reserved.
-            </div>
-         </div>
-      </footer>
-   );
+        </footer>
+    );
 };
