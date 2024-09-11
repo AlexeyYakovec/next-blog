8ee2efe82eb166e8784e337fe7c17eb5e91d6869
unknown <yakovec8952@gmail.com>
2024-09-11 15:51:06 +0500

    finished app

--- app/blog/[category]/[slug]/page.tsx
+++ app/blog/[category]/[slug]/page.tsx
@@ -1,63 +1,101 @@
-import { notFound } from "next/navigation";
-import { formatDate, getBlogPosts } from "../../utils";
-import { Container, Header } from "@/components/ui";
-import { Breadcrumb } from "@/components/ui/breadcrumb";
-import { BreadcrumbWithCustomSeparator } from "@/components/shared/breadcrumb";
-import { CustomMDX } from "@/components/shared/mdx";
-import { ReportView } from "@/components/ui/report-view";
+import { notFound } from 'next/navigation';
+import { formatDate, getBlogPosts } from '../../utils';
+import { Container, Header } from '@/components/ui';
+import { Breadcrumb } from '@/components/ui/breadcrumb';
+import { BreadcrumbWithCustomSeparator } from '@/components/shared/breadcrumb';
+import { CustomMDX } from '@/components/shared/mdx';
+import { ReportView } from '@/components/ui/report-view';
+import { baseUrl } from '@/app/sitemap';
 
 export async function generateStaticParams() {
-   let posts = getBlogPosts();
+    let posts = getBlogPosts();
 
-   return posts.map((post) => {
-      slug: post.slug;
-   });
+    return posts.map((post) => {
+        slug: post.slug;
+    });
+}
+
+export function generateMetadata({
+    params,
+}: {
+    params: { slug: string; category: string };
+}) {
+    let post = getBlogPosts().find((post) => post.slug === params.slug);
+    console.log(post?.metadata);
+
+    if (!post) {
+        return;
+    }
+
+    let {
+        title,
+        publishedAt: publishedTime,
+        summary: description,
+        image,
+    } = post.metadata;
+
+    let ogImage = image
+        ? image
+        : `${baseUrl}/og?title=${encodeURIComponent(title)}`;
+
+    return {
+        title,
+        description,
+        openGraph: {
+            title,
+            description,
+            type: 'article',
+            publishedTime,
+            url: `${baseUrl}/blog/${post?.metadata.category}/${post?.slug}}`,
+            images: [{ url: ogImage }],
+        },
+    };
 }
 
 interface PageProps {
-   params: {
-      category: string;
-      slug: string;
-   };
+    params: {
+        category: string;
+        slug: string;
+    };
 }
 
 export default function Page({ params }: PageProps) {
-   let post = getBlogPosts().find((post) => post.slug === params.slug);
+    let post = getBlogPosts().find((post) => post.slug === params.slug);
 
-   if (!post) {
-      notFound();
-   }
+    if (!post) {
+        notFound();
+    }
 
-   return (
-      <>
-         <ReportView
-            category={post.metadata.category}
-            title={post.metadata.title}
-            slug={post.slug}
-         />
-         <Header>
+    return (
+        <>
+            <ReportView
+                category={post.metadata.category}
+                title={post.metadata.title}
+                slug={post.slug}
+            />
+            <Header>
+                <Container>
+                    <BreadcrumbWithCustomSeparator
+                        category={post.metadata.category}
+                        slug={post.slug}
+                    />
+                    <h1 className="title font-semibold text-2xl tracking-tighter mt-4">
+                        {post.metadata.title}
+                    </h1>
+
+                    <div className="flex justify-between items-center mt-2 mb-4 text-sm">
+                        <p className="text-sm text-neutral-600 dark:text-neutral-400 mt-2">
+                            {formatDate(post.metadata.publishedAt)}
+                        </p>
+                    </div>
+                </Container>
+            </Header>
+
             <Container>
-               <BreadcrumbWithCustomSeparator
-                  category={post.metadata.category}
-                  slug={post.slug}
-               />
-               <h1 className="title font-semibold text-2xl tracking-tighter mt-4">
-                  {post.metadata.title}
-               </h1>
-
-               <div className="flex justify-between items-center mt-2 mb-4 text-sm">
-                  <p className="text-sm text-neutral-600 dark:text-neutral-400 mt-2">
-                     {formatDate(post.metadata.publishedAt)}
-                  </p>
-               </div>
+                <article className="prose">
+                    <CustomMDX source={post.content} />
+                </article>
             </Container>
-         </Header>
-
-         <Container>
-            <article className="prose">
-               <CustomMDX source={post.content} />
-            </article>
-         </Container>
-      </>
-   );
+        </>
+    );
 }
--- components/ui/footer.tsx
+++ components/ui/footer.tsx
@@ -91,6 +91,13 @@ export const Footer: React.FC<Props> = ({ className }) => {
                                     Privacy Policy
                                 </Link>
                             </li>
+                            <li>
+                                <Link
+                                    href={'/sitemap.xml'}
+                                    className="text-gray-500 hover:text-gray-700 dark:text-gray-400 dark:hover:text-gray-300 transition-all">
+                                    Sitemap
+                                </Link>
+                            </li>
                         </ul>
                     </div>
 
